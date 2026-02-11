df011e8066bbe58d032e0d6cb1c5a0ca061a2b73
[Recovery Eyo} <eyorecovery95@gmail.com>
2026-02-11 01:30:24 +0100

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { navLinks } from "../constants";
+
+const Navbar = () => {
+  return (
+    <header>
+      <nav>
+        <img src="/logo.svg" alt="logo" />
+        <ul>
+          {navLinks.map((item, index) => (
+            <li key={index}>
+              <a href={item.href}>{item.label}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="flex-center gap-3">
+          <button>
+            <img src="/search.svg" alt="search" />
+          </button>
+          <button>
+            <img src="/cart.svg" alt="cart" />
+          </button>
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
---
+++ src/constants/index.js
@@ -0,0 +1,7 @@
+export const navLinks = [
+  { label: "store", href: "#" },
+  { label: "iphone", href: "#" },
+  { label: "watch", href: "#" },
+  { label: "ipad", href: "#" },
+  { label: "mac", href: "#" },
+];
