987260cb04ad06f1cec4738992af559710c84184
[Recovery Eyo} <eyorecovery95@gmail.com>
2026-02-12 13:40:35 +0100

    implemented the feature section

---
+++ src/components/models/ModelScroll.jsx
@@ -0,0 +1,95 @@
+import { Html } from "@react-three/drei";
+import React, { Suspense, useEffect, useRef } from "react";
+import { useMediaQuery } from "react-responsive";
+import MacbookModel from "./Macbook";
+import useMacbookStore from "../../store";
+import { useGSAP } from "@gsap/react";
+import { featureSequence } from "../../constants";
+import gsap from "gsap";
+
+const ModelScroll = () => {
+  const groupRef = useRef(null);
+  const isMobile = useMediaQuery({ query: "(max-width: 1024px)" });
+  const { setTexture } = useMacbookStore();
+
+  //pre-load all feature videos during component mount
+  useEffect(() => {
+    featureSequence.forEach((feature) => {
+      const v = document.createElement("video");
+
+      Object.assign(v, {
+        src: feature.videoPath,
+        muted: true,
+        playsInline: true,
+        preload: "auto",
+        loop: true,
+        crossorigin: "anonymous",
+      });
+
+      v.load();
+    });
+  }, []);
+
+  useGSAP(() => {
+    //3d model rotation animation
+    const modelTimeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#f-canvas",
+        start: "top top",
+        end: "bottom top",
+        scrub: 1,
+        pin: true,
+      },
+    });
+
+    //sync feature content
+
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#f-canvas",
+        start: "top center",
+        end: "bottom top",
+        scrub: 1,
+      },
+    });
+
+    //3d spin
+    if (groupRef.current) {
+      modelTimeline.to(groupRef.current.rotation, {
+        y: Math.PI * 2,
+        ease: "power1.inOut",
+      });
+    }
+
+    timeline
+      .call(() => setTexture("/videos/feature-1.mp4"))
+      .to(".box1", { opacity: 1, y: 0, delay: 1 })
+
+      .call(() => setTexture("/videos/feature-2.mp4"))
+      .to(".box2", { opacity: 1, y: 0 })
+
+      .call(() => setTexture("/videos/feature-3.mp4"))
+      .to(".box3", { opacity: 1, y: 0 })
+
+      .call(() => setTexture("/videos/feature-4.mp4"))
+      .to(".box4", { opacity: 1, y: 0 })
+
+      .call(() => setTexture("/videos/feature-5.mp4"))
+      .to(".box5", { opacity: 1, y: 0 });
+  });
+  return (
+    <group ref={groupRef}>
+      <Suspense
+        fallback={
+          <Html>
+            <h1 className="uppercase text-white text-3xl">Loading...</h1>
+          </Html>
+        }
+      >
+        <MacbookModel scale={isMobile ? 0.05 : 0.08} position={[0, -1, 0]} />
+      </Suspense>
+    </group>
+  );
+};
+
+export default ModelScroll;
--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,12 @@ import { create } from "zustand";
 const useMacbookStore = create((set) => ({
   color: "#2e2c2e",
   scale: 0.08,
+  texture: "/videos/feature-1.mp4",
   setColor: (color) => set({ color }),
   setScale: (scale) => set({ scale }),
-
-  reset: () => set({ color: "#2e2c2e", scale: 0.08 }),
+  setTexture: (texture) => set({ texture }),
+  reset: () =>
+    set({ color: "#2e2c2e", scale: 0.08, texture: "/videos/feature-1.mp4" }),
 }));
 
 export default useMacbookStore;
